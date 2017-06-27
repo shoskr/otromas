aa942163c1667296ab24897f12b81cf03b5f3407
oscar <oscar@10.128.40.139>
2017-06-27 12:21:29 -0400

    oscar

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -27,24 +27,23 @@ class loginController extends Controller {
                 $id = $us->id_usuario;
             }
         }
-             echo $id;
+      
         if (password_verify($request->password, $pasR)) {
             switch ($tipo) {
                 case 1:
                     session_start();
                     $_SESSION['user'] = $user;
-                    $_SESSION['id']=$id;
+                    $_SESSION['id'] = $id;
                     return \View::make('AlumFrom');
                     break;
                 case 2:
                     session_start();
                     $_SESSION['user'] = $user;
-                    return \View::make('formSecretaria');            
+                    return \View::make('formSecretaria');
                 case 3:
                     session_start();
+                    Alert::info('Bienbenido','info' );
                     $_SESSION['user'] = $user;
-                    
-                    $_GET["mensaje"]= 'Bienvenido :' .$user;
                     return \View::make('formUTP', compact($_SESSION['user']));
                 case 4:
                     session_start();
