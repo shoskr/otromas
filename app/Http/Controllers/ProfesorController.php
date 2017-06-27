7b15c9f3069a9d5bd64e44f3225736bee1d55a2d
oscar <oscar@10.128.40.139>
2017-06-27 01:39:53 -0400

    Profesor Guarda,lista,busca,elimina

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -3,16 +3,16 @@
 namespace colegio\Http\Controllers;
 
 use Illuminate\Http\Request;
+use colegio\Profesor as Profesor;
 
-class ProfesorController extends Controller
-{
+class ProfesorController extends Controller {
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         //
     }
 
@@ -21,8 +21,7 @@ class ProfesorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
@@ -32,20 +31,69 @@ class ProfesorController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request) {
+
+        function agregar_dv($_rol) {
+            while ($_rol[0] == "0") {
+                $_rol = substr($_rol, 1);
+            }
+            $factor = 2;
+            $suma = 0;
+            for ($i = strlen($_rol) - 1; $i >= 0; $i--) {
+                $suma += $factor * $_rol[$i];
+                $factor = $factor % 7 == 0 ? 2 : $factor + 1;
+            }
+            $dv = 11 - $suma % 11;
+            /* Por alguna razón me daba que 11 % 11 = 11. Esto lo resuelve. */
+            $dv = $dv == 11 ? 0 : ($dv == 10 ? "K" : $dv);
+            return $dv;
+        }
+
+        $profesor = new Profesor;
+        $profesor->rut = $request->rut;
+
+        $al = agregar_dv($request->rut);
+        $dv = $request->dv;
+
+        $profesor->nombre_profesor = $request->nombre;
+        $profesor->fecha_contrato = $request->fechac;
+        $profesor->asignatura = $request->asignatura;
+        $profesor->valor_tutoria = $request->valor;
+        $profesor->estado = 1;
+
+        if ($al == $dv) {
+            $profesor->save();
+            session_start();
+            $_SESSION['rut'] = $request->rut;
+            return \View::make('ListaProfesor', compact($_SESSION['rut']));
+        } else {
+            session_start();
+            return \View::make('formUTP');
+        }
     }
 
+    public function listAll() {
+        $profesor = Profesor::all();
+        session_start();
+        return \View::make('ListaProfesor', compact('profesor'));
+    }
+ public function listAll2() {
+        $profesor = Profesor::all();
+        session_start();
+        return \View::make('elimProfesor', compact('profesor'));
+    }
     /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
+    public function show(Request $request) {
+
+        $pro = Profesor::all()->where('rut', '=', $request->rut);
+        session_start();
+
+        return \View::make('consProfesor', compact('pro'));
     }
 
     /**
@@ -54,8 +102,7 @@ class ProfesorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         //
     }
 
@@ -66,8 +113,7 @@ class ProfesorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         //
     }
 
@@ -77,8 +123,11 @@ class ProfesorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
-        //
+    public function destroy($id) {
+        $profesor = Profesor::where('rut', $id);
+        $profesor->delete();
+        session_start();
+        return redirect()->back();
     }
+
 }
